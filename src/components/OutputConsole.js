ce73a40717252bea991b5eab444edf6d9388909a
Tanisha Bisht <agamyatani@gmail.com>
2024-11-10 00:42:52 -0500

    add outputConsole css

--- src/components/OutputConsole.js
+++ src/components/OutputConsole.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const OutputConsole = ({ output }) => {
   return (
-    <div style={{ padding: '10px', background: '#f5f5f5', marginTop: '10px', maxHeight: '100px', overflowY: 'auto' }}>
-      <h3>Output Console</h3>
-      <pre>{output}</pre>
+    <div className="output-console">
+      <h3 className="output-console__title">Output Console</h3>
+      <pre className="output-console__text">{output}</pre>
     </div>
   );
 };
